80e0e49f9618830c0d405ed4cd89f4b6fb23846c
aliarjomandbigdeli <aliabigdeli@gmail.com>
2019-01-21 15:00:48 +0330

    add split_channel_blue, show_gray_image, rotate_image, half_width_image,resize_image

--- run.py
+++ run.py
@@ -1,15 +1,65 @@
+import math
+
 import cv2
 
 
-def read_and_show_image(file_name):
-    color = cv2.imread(file_name, 1)
-    cv2.imshow(file_name, color)
+def main():
+    file_name = "test.jpg"
+
+    read_and_show_image(file_name)
+
+    split_channel_blue(file_name)
+
+    show_gray_image(file_name)
+
+    rotate_image(file_name, 90)
+
+    half_width_image(file_name)
+
+    img = cv2.imread(file_name, 1)
+    height, width, channels = img.shape
+    resize_image(file_name, math.floor(width / 2), height)
+
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
 
-def main():
-    read_and_show_image("test.jpg")
+def read_and_show_image(file_name):
+    color = cv2.imread(file_name, 1)
+    cv2.imshow("image", color)
+
+
+def split_channel_blue(file_name):
+    color = cv2.imread(file_name, 1)
+    b, g, r = cv2.split(color)
+    b_split = cv2.merge([b, b, b])
+    cv2.imshow("blue", b_split)
+
+
+def show_gray_image(file_name):
+    color = cv2.imread(file_name, 1)
+    gray = cv2.cvtColor(color, cv2.COLOR_RGB2GRAY)
+    cv2.imwrite("gray.jpg", gray)
+    cv2.imshow("gray scale", gray)
+
+
+def rotate_image(file_name, angel):
+    img = cv2.imread(file_name, 1)
+    M = cv2.getRotationMatrix2D((img.shape[1] / 2, img.shape[0] / 2), angel, 1)
+    rotated = cv2.warpAffine(img, M, (img.shape[1], img.shape[0]))
+    cv2.imshow("rotated", rotated)
+
+
+def half_width_image(file_name):
+    img = cv2.imread(file_name, 1)
+    resize_image = cv2.resize(img, (0, 0), fx=0.5, fy=1)
+    cv2.imshow("resize image", resize_image)
+
+
+def resize_image(file_name, width, height):
+    img = cv2.imread(file_name, 1)
+    resize_image = cv2.resize(img, (width, height))
+    cv2.imshow("resize image 2", resize_image)
 
 
 if __name__ == '__main__': main()
