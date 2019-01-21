2d9904825f4ffbd3a53399ae6a85110fba778bd0
aliarjomandbigdeli <aliabigdeli@gmail.com>
2019-01-21 19:08:39 +0330

    add some comments

--- run.py
+++ run.py
@@ -103,19 +103,21 @@ def show_image_edges(file_name):
 
 def segment_image(file_name):
     img = cv2.imread(file_name)
-    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     ret, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
 
-    # removing noise
+    # noise removal
     kernel = np.ones((3, 3), np.uint8)
     opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel, iterations=2)
 
-    # finding sure foreground area
+    # sure background area
     sure_bg = cv2.dilate(opening, kernel, iterations=3)
+
+    # Finding sure foreground area
     dist_transform = cv2.distanceTransform(opening, cv2.DIST_L2, 5)
     ret, sure_fg = cv2.threshold(dist_transform, 0.7 * dist_transform.max(), 255, 0)
 
-    # finding unknown region
+    # Finding unknown region
     sure_fg = np.uint8(sure_fg)
     unknown = cv2.subtract(sure_bg, sure_fg)
 
